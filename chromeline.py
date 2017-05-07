2cd20615a58fea774b556bf20ca9ad2f8cbdb506
Robert Wallhead <rwallhead@gmail.com>
2017-05-07 12:06:04 +1000

    Successful connect/disconnect logic implementation
    No code survives first contact with real hardware :)

--- chromeline.py
+++ chromeline.py
@@ -1,5 +1,7 @@
 """Cast a Pi line in to a Chromecast."""
 import ConfigParser
+import os
+import signal
 import subprocess
 import time
 
@@ -20,7 +22,7 @@ def get_chromecast(uuid):
 def enable_stream(audio_device, password):
     """Start the stream, restart it on error."""
     cmd = ['./push_to_icecast.sh', audio_device, password]
-    return subprocess.Popen(cmd)
+    return subprocess.Popen(cmd, preexec_fn=os.setsid)
 
 
 def cast_stream(chromecast, stream_source_ip, mount='chromeline.ogg'):
@@ -41,8 +43,13 @@ def enable_pulseaudio():
     subprocess.call(['pulseaudio', '-D'])
 
 
+def chromecast_still_connected(chromecast):
+    """Seems to be the bulletproof way to work this out."""
+    return not chromecast.socket_client.heartbeat_controller.is_expired()
+
+
 def chromeline(chromecast_uuid, stream_source_ip, line_in_device, icecast_password):
-    """The main loop."""
+    """Connect it all up."""
     chromecast = None
     while chromecast is None:
         print 'Searching for Chromecast...'
@@ -51,21 +58,39 @@ def chromeline(chromecast_uuid, stream_source_ip, line_in_device, icecast_passwo
 
     print 'Mounting internal stream in Icecast...'
     process = enable_stream(line_in_device, icecast_password)
+    time.sleep(5)
 
     print 'Casting external stream to Chromecast...'
     cast_stream(chromecast, stream_source_ip)
 
     print 'Running...'
-    return process.wait()
+    return chromecast, process
 
 
-if __name__ == '__main__':
-
-    conf = load_config()
+def main():
+    """Main loop."""
     enable_pulseaudio()
 
     while True:
-        code = chromeline(**conf)
-        if code != 0:
+
+        conf = load_config()
+        chromecast, process = chromeline(**conf)
+
+        while True:
             time.sleep(5)
-            continue
+
+            if not chromecast_still_connected(chromecast):
+                print 'Stopping internal stream due to Chromecast disconnect!'
+                os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+                process.kill()
+                break
+
+            if process.poll() is not None:
+                print 'Restarting due to internal stream failing!'
+                break
+
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
